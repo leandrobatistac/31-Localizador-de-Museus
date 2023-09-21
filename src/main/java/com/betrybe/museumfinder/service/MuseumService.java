108e2cd50fbfef74c0b45012eb38c9fdc3791118
leandrobatista <leandrobcrv@gmail.com>
2023-09-21 14:52:29 -0300

    Requisito 03

---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -0,0 +1,36 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MuseumService implements MuseumServiceInterface {
+  private final MuseumFakeDatabase museumFakeDatabase;
+
+  public MuseumService(MuseumFakeDatabase museumFakeDatabase) {
+    this.museumFakeDatabase = museumFakeDatabase;
+  }
+
+  @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    return null;
+  }
+
+  @Override
+  public Museum createMuseum(Museum museum) {
+    Coordinate coordinate = museum.getCoordinate();
+    if (!CoordinateUtil.isCoordinateValid(coordinate)){
+      throw new InvalidCoordinateException();
+    }
+    return museumFakeDatabase.saveMuseum(museum);
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    return null;
+  }
+}
